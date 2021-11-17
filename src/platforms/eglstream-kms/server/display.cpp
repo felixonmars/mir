b234cc1b8b731ee304d9cea18744e5cab9cc48c3
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2021-11-18 10:16:31 +1100

    REBASE: Temp

--- src/platforms/eglstream-kms/server/display.cpp
+++ src/platforms/eglstream-kms/server/display.cpp
@@ -122,8 +122,7 @@ EGLContext create_context(EGLDisplay display, EGLConfig config, EGLContext share
 
 class DisplayBuffer
     : public mg::DisplaySyncGroup,
-      public mg::DisplayBuffer,
-      public mir::renderer::gl::RenderTarget
+      public mg::DisplayBuffer
 {
 public:
     DisplayBuffer(
@@ -224,7 +223,7 @@ public:
         return view_area_;
     }
 
-    bool overlay(const mir::graphics::RenderableList& /*renderlist*/) override
+    bool overlay(std::vector<mg::DisplayElement> const&) override
     {
         return false;
     }
